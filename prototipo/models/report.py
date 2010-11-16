3dbd3fdd12fdc3fad8a1f526bb1726d332cc2e8e
Gabriel Gayan <gabrielgayan@gmail.com>
2010-11-16 15:33:32 -0300

    agrego la descarga de entregas pasadas

--- prototipo/models/report.py
+++ prototipo/models/report.py
@@ -8,11 +8,11 @@ import gdata.docs.client
 import gdata.spreadsheet.service
 import settings
 
-'''
-Descripcion de una de las entregas de cierto grupo
-y de su feedback asociado
-'''
 class Report(models.Model):
+    '''
+    Descripcion de una de las entregas de cierto grupo
+    y de su feedback asociado
+    '''
     description = models.ForeignKey('ReportDescription')
     group = models.ForeignKey(Group)
     blocked = models.BooleanField()
@@ -21,7 +21,11 @@ class Report(models.Model):
     last_delivery_date = models.DateTimeField(blank = True, null = True)
     first_correction_date = models.DateTimeField(blank = True, null = True)
     validation_date = models.DateTimeField(blank = True, null = True)
-    
+
+    @property
+    def delivered(self):
+        return self.last_delivery_date < self.description.delivery_end_date
+
     @staticmethod
     def generate_report(report_description, group):
         report = Report()
@@ -29,7 +33,7 @@ class Report(models.Model):
         report.group = group
         report.blocked = True
         report.corrected = False
-        
+
         document_url = upload_file(report.description, str(report.description.course_instance) + ' - ' + report.description.name + ' - ' + report.group.name)
         report.feedback_key = get_gdoc_key(document_url)
         report.share_with_assistant()
@@ -39,30 +43,29 @@ class Report(models.Model):
 
     def store_file(self, uploaded_file):
         store_file(uploaded_file, 'uploaded_reports', self.id, ['.zip'])
-        
+
     def share_with_assistant(self):
         self.share('writer', self.group.assistant.person.email)
-        
+
     def share_with_auxiliary(self):
         self.share('reader', self.group.auxiliary.person.email)
 
     def share_with_coordinator(self):
         self.share('writer', self.group.coordinator.email)
-        
+
     def share(self, permissions, email):
         gd_client = gdata.docs.service.DocsService()
         gd_client.ClientLogin(settings.ACCOUNT_EMAIL, settings.ACCOUNT_PASSWORD)
         feed_url = 'https://docs.google.com/feeds/acl/private/full/spreadsheet%3A' + self.feedback_key
-        
+
         scope = gdata.docs.Scope(value = email, type = 'user')
         role = gdata.docs.Role(value = permissions)
         acl_entry = gdata.docs.DocumentListAclEntry(scope = scope, role = role)
 
         created_acl_entry = gd_client.Post(acl_entry, feed_url, converter = gdata.docs.DocumentListAclEntryFromString)
-        
-        
+
     def __unicode__(self):
         return unicode(self.description) + ' - ' + unicode(self.group)
-    
+
     class Meta:
         app_label = 'prototipo'
